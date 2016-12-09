5fc7a757b28aa6a3e2077fcf855d881b56c80057
Ibrahim Imran Saeed <imran.saeed@ooredoo.mv>
2016-12-09 06:12:22 +0500

    Home click event

--- app/js/Controllers/HomeController.js
+++ app/js/Controllers/HomeController.js
@@ -1,8 +1,12 @@
 (function () {
-	var Controller = function (NewsService) {
+	var Controller = function (NewsService, $location) {
 		var ctrl = this;
 
 		ctrl.topArticles = NewsService.getTopArticles();
+
+		ctrl.openNews = function (article) {
+			$location.path('/news/' + article.id);
+		}
 	}
 
 	angular.module('NewsRoom').controller('HomeController', Controller);
